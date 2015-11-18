7f7d0df7ffada881cc0ab533ad91f36d4e150ceb
Adriano Machado <adriano@epicom.com.br>
2015-11-18 17:48:33 -0200

    Fixes #9

--- ngplus-overlay.js
+++ ngplus-overlay.js
@@ -173,7 +173,7 @@
                 scope.show = false;
             }
 
-            var getComputedStyle = function () {
+            var getComputedStyle = (function () {
                 var func = null;
                 if (document.defaultView && document.defaultView.getComputedStyle) {
                     func = document.defaultView.getComputedStyle;
@@ -181,12 +181,38 @@
                     func = function (element, anything) {
                         return element["currentStyle"];
                     };
+                } else {
+                    // Polyfill for getComputedStyle from: https://gist.github.com/twolfson/5369885
+                    func = function (el, prop, getComputedStyle) {
+                        getComputedStyle = window.getComputedStyle;
+
+                        // In one fell swoop
+                        return (
+                            // If we have getComputedStyle
+                            getComputedStyle ?
+
+                            // Query it
+                            // TODO: From CSS-Query notes, we might need (node, null) for FF
+                            getComputedStyle(el) :
+
+                            // Otherwise, we are in IE and use currentStyle
+                            el.currentStyle
+                        )[
+                            // Switch to camelCase for CSSOM
+                            // DEV: Grabbed from jQuery
+                            // https://github.com/jquery/jquery/blob/1.9-stable/src/css.js#L191-L194
+                            // https://github.com/jquery/jquery/blob/1.9-stable/src/core.js#L593-L597
+                            prop.replace(/-(\w)/gi, function (word, letter) {
+                                return letter.toUpperCase();
+                            })
+                        ];
+                    };
                 }
 
                 return function (element, style) {
                     return func(element, null)[style];
-                }
-            }();
+                };
+            })();
         }
     }
 }());
